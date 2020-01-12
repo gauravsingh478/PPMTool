9c9027c2838a06c06a9bb6fd77302834f8b48194
gauravsingh478 <gauravsingh478knp@gmail.com>
2020-01-12 12:33:07 +0530

    first commit

---
+++ ppmtool/src/test/java/com/greatglory/ppmtool/PpmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greatglory.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
