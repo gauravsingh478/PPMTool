d24c3990554e60dfbf540bb4ccee5cccf8cb6642
gauravsingh478 <gauravsingh478knp@gmail.com>
2020-01-13 15:09:32 +0530

    Complete Project Object

--- ppmtool/src/main/java/com/greatglory/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/com/greatglory/ppmtool/domain/Project.java
@@ -1,4 +1,42 @@
 package com.greatglory.ppmtool.domain;
 
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date start_date;
+    private Date end_date;
+
+    private Date created_At;
+    private Date updated_At;
+
+    public Project(){
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updated_At = new Date();
+    }
 }
---
+++ ppmtool/src/main/java/com/greatglory/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,10 @@
+package com.greatglory.ppmtool.repositories;
+
+
+import com.greatglory.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+    
+}
