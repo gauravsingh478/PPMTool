16e5cf535ba351a8676208ab0562801e888e1146
gauravsingh478 <gauravsingh478knp@gmail.com>
2020-01-14 17:10:02 +0530

    Mapped error's list in controller class

---
+++ ppmtool/src/main/java/com/greatglory/ppmtool/services/MapValidatorErrorService.java
@@ -0,0 +1,19 @@
+package com.greatglory.ppmtool.services;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class MapValidatorErrorService {
+    public ResponseEntity<?> MapValidation(BindingResult result){
+
+        return null;
+
+    }
+}
