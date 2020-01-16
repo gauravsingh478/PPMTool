a9b58d6142b2f2983cd0cea717ebafc02ffa5116
gauravsingh478 <gauravsingh478knp@gmail.com>
2020-01-16 12:19:11 +0530

    Created 3 new exception class for handling error exceptions for database

--- ppmtool/src/main/java/com/greatglory/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/greatglory/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.greatglory.ppmtool.services;
 
 import com.greatglory.ppmtool.domain.Project;
+import com.greatglory.ppmtool.exceptions.ProjectIdException;
 import com.greatglory.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,15 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
     public Project saveOrUpdateProject( Project project){
-        return projectRepository.save(project);
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Prject id ' "+project.getProjectIdentifier().toUpperCase()+" ' already exists");
+        }
+
+
+
     }
 
 }
